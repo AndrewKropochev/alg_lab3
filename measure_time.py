e249ec2a06921115eeb428ad5fc1550896e2a2a5
AndrewKropochev <45901398+AndrewKropochev@users.noreply.github.com>
2019-05-22 17:03:02 +0700

    Add files via upload

--- measure_time.py
+++ measure_time.py
@@ -7,7 +7,8 @@ from tqdm import tqdm
 from statistics import mean
 import random
 from search import linear_search, binary_search
-
+from random import gauss
+from matplotlib import pyplot
 
 def generate_sorted_data(sz):
     """
@@ -35,14 +36,39 @@ def measure_search_time(search_alg, sz, repeats):
     return mean(results)
 
 
+#def main():
+#    
+#    valsA = range(10000,100000,10000)
+#
+#    valsB = []
+#    for i in valsA:
+#        valsB.append(measure_search_time(linear_search, i, 500))
+#
+#        
+#    valsC = []
+#    for i in valsA:
+#        valsC.append(measure_search_time(binary_search, i, 500))
+#    
+#    pyplot.scatter(valsA, valsB, s=40, marker='.')
+#    pyplot.scatter(valsA, valsC, s=40, marker='.')
+#    pyplot.show()
+
 def main():
     sizes = []
     avg_time = []
+    avg2_time = []
+    
+    
     for sz in tqdm(range(10, 50000, 1000)):
         sizes.append(sz)
         avg_time.append(measure_search_time(linear_search, sz, 500))
+        avg2_time.append(measure_search_time(binary_search, sz, 500))
+    
     plt.plot(sizes, avg_time)
+    plt.plot(sizes, avg2_time)
+
     plt.show()
+
 
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -12,6 +12,26 @@ def linear_search(data, value):
 
 
 def binary_search(data, value):
-    # Реализуйте алгоритм здесь
-    pass
+    mid = len(data) // 2
+    low = 0
+    high = len(data) - 1
+     
+    while data[mid] != value and low <= high:
+        if value > data[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+        mid = (low + high) // 2
+     
+    if low > high:
+        return None
+    else:
+        return mid
+
+
+    """
+    тест
+    """
+gag = [1,5,34,45,55,56,67,79,88,887,895,910,1000]
+print(binary_search(gag,34))
 
